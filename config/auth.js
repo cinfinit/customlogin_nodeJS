58f8ea7edc6687af15f9a1b1184f67b39867c4c0
cinfinit <39793609+cinfinit@users.noreply.github.com>
2018-06-30 11:37:50 +0530

    Update auth.js

--- config/auth.js
+++ config/auth.js
@@ -2,16 +2,16 @@ module.exports = {
   facebookAuth: {
    clientID: process.env.fbClientID,
    clientSecret: process.env.fbClientSecret,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: 'https://somethingmongo.herokuapp.com/auth/facebook/callback',
   },
   twitterAuth: {
     consumerKey: process.env.twitterClientID,
     consumerSecret: process.env.twitterClientSecret,
-    callbackURL: 'http://localhost:3000/auth/twitter/callback',
+    callbackURL: 'https://somethingmongo.herokuapp.com/auth/twitter/callback',
   },
   googleAuth: {
     clientID: process.env.googleClientID,
     clientSecret: process.env.googleClientSecret,
-    callbackURL: 'http://localhost:3000/auth/google/callback',
+    callbackURL: 'https://somethingmongo.herokuapp.com/auth/google/callback',
   },
 };
